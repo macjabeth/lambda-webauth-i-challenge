e977b97f6ded0d9e6af98e3ce7c5c758929782d4
macjabeth <macbethjonathan@gmail.com>
2019-04-01 16:43:49 -0400

    setup root index and server middleware

---
+++ api/server.js
@@ -0,0 +1,26 @@
+const debug = require('debug')('server:log');
+const compression = require('compression');
+const helmet = require('helmet');
+const express = require('express');
+const server = express();
+
+// Middleware
+server.use(express.json());
+server.use(compression());
+server.use(helmet());
+server.use((req, res, next) => {
+  res.on('finish', () =>
+    debug(`${req.method} ${req.originalUrl} - ${res.statusCode} [${res.statusMessage}]`));
+  next();
+});
+
+// Routes
+server.use('/api/register', require('../routes/register'));
+server.use('/api/login', require('../routes/login'));
+server.use('/api/users', require('../routes/users'));
+
+server.use('/api', (req, res) => {
+  res.status(418).json({ message: "It's working! It's working!!!" });
+});
+
+module.exports = server;
