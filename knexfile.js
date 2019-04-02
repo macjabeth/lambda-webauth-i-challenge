16137e0360c8c0dd51b77366b9336b461460d030
macjabeth <macbethjonathan@gmail.com>
2019-04-02 18:32:32 -0400

    update index and knex files to reflect config change

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,1 @@
-module.exports = Object.assign({},
-  require('config').get('knex'), {
-  pool: { // *grumble*
-    afterCreate: (conn, done) => {
-      conn.run('PRAGMA foreign_keys = ON', done);
-    }
-  }
-});
+module.exports = require('./config/knex');
