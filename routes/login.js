9e31c68db9767f86ed1f9b2a5f60d3f611b11441
macjabeth <macbethjonathan@gmail.com>
2019-04-01 20:31:45 -0400

    add joi validation to login/register routes

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,22 @@
+const Joi = require('joi');
 const debug = require('debug')('server:db');
 const router = require('express').Router();
 const userDB = require('../models/users');
 const bcrypt = require('bcryptjs');
 
-router.post('/', async ({ body: { username, password } }, res) => {
+const schema = Joi.object().keys({
+  username: Joi.string().max(35).required(),
+  password: Joi.string().max(128).required()
+});
+
+router.post('/', async ({ body: creds }, res) => {
+  const { error } = Joi.validate(creds, schema);
+  if (error) return res.status(400).json({
+    error: error.details[0].message
+  });
+
   try {
+    const { username, password } = creds;
     const [user] = await userDB.findBy({ username });
     user && bcrypt.compareSync(password, user.password)
       ? res.status(200).json({ message: 'Logged In' })
--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,20 @@
+const Joi = require('joi');
 const debug = require('debug')('server:db');
 const router = require('express').Router();
 const userDB = require('../models/users');
 const bcrypt = require('bcryptjs');
 
+const schema = Joi.object().keys({
+  username: Joi.string().max(35).required(),
+  password: Joi.string().max(128).required()
+});
+
 router.post('/', async ({ body: newUser }, res) => {
+  const { error } = Joi.validate(newUser, schema);
+  if (error) return res.status(400).json({
+    error: error.details[0].message
+  });
+
   try {
     newUser.password = bcrypt.hashSync(newUser.password, 4);
     const [user] = await userDB.add(newUser);
